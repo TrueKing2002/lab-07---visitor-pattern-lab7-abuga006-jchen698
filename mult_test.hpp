df6abe6698f0bc45428088d6988cd0f0eda97898
jc51001 <jchen698@ucr.edu>
2022-03-03 12:26:10 -0800

    Fixed so that make works and all tests pass

--- mult_test.hpp
+++ mult_test.hpp
@@ -21,6 +21,6 @@ TEST(MultTest, ChildRight) {
     Op* left = new Op(10);
     Base* right = new Op(20);
     Mult* test = new Mult(left, right);
-    EXPECT_EQ(test->get_child(1), left);
+    EXPECT_EQ(test->get_child(1), right);
 }
 
